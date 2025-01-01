168aaf5bea6c24b2f748152b3393e611aef10573
Swatson-jnr <jkad.swatsonjnr@gmail.com>
2025-01-01 21:44:39 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary:{
+          100: '#E2E205',
+          200: '#888883'
+        },
+        success: {
+          100: '#1A202C',
+          200: '#2D3748',
+          300: '#212529',
+        } 
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
